0ad2eb3706751270d50816537dd4c3446b700fef
joeListro <jlistro@gmail.com>
2016-10-04 14:20:47 -0400

    Changed orginization

---
+++ src/producer_consumer.h
@@ -0,0 +1,19 @@
+/*
+ * Filename: producer_consumer.h
+ * Author: Bryan Martin
+ * Class: CSI-385-02
+ * Assignment: Lab 2
+ * 
+ * Date-Assigned: 9/29/2016
+ * Due-Date: 9/5/2016
+ * 
+ * Description: The header file of the producer_consumer program. Contains global variables[:(] and function definitions.
+ * 
+ * Certification of Authenticity: I certify that this assignment is entirely my own work.
+ *
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
